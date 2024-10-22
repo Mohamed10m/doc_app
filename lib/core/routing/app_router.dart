231d3a9d8ba2618eb1ca4fd820247c474a887d46
Mohamed Mahmoud <mohamedelshenawy629@gmail.com>
2024-10-22 12:26:11 -0700

    update

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -14,7 +14,6 @@ import '../di/dependency_injection.dart';
 class AppRouter {
   Route? generateRoute(RouteSettings settings) {
     //this arguments to be passed in any screen like this ( arguments as ClassName )
-    final arguments = settings.arguments;
 
     switch (settings.name) {
       case Routes.onBoardingScreen:
